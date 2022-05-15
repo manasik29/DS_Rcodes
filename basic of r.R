45eb184c1ab5b8922fb996db243e212b2614cce7
manasik29 <97607775+manasik29@users.noreply.github.com>
2022-05-15 13:04:16 +0530

    Add files via upload

---
+++ basic of r.R
@@ -0,0 +1,107 @@
+mean(Q7$Points)
+mean(Q7$Score)
+mean(Q7$Weigh)
+
+0.015+0.20+0.65+.005+0.01+0.120
+(0.015)+(4*.20)+(3*0.65)+(5*0.005)+(6*0.01)+(2*0.120)
+  summary(Q7)
+mode(Q7$Points)
+mode(Q7$Score)
+mode(Q7$Weigh)
+var(Q7$Points)
+
+var(Q7$Score)
+var(Q7$Weigh)
+range(Q7$Points)
+range(Q7$Score)
+range(Q7$Weigh)
+4.93-2.76
+5.424-1.513
+22.9-14.5
+hist(Q7$Points)
+hist(Q7$Score)
+hist(Q7$Weigh)
+glimpse(Cars)
+mpg<-Cars$MPG
+mpg
+filter(Cars, MPG>38)
+filter(Cars, MPG<40)
+Cars%>%
+  filter(MPG<50, MPG>20)
+hist(Cars$MPG)
+line(Cars$MPG)
+barplot(Cars$MPG)
+sd(Cars$MPG)
+skew(Cars$MPG)
+
+
+#09/02/22
+m=T
+n=F
+class(m)
+class(n)
+
+x = TRUE
+y = 'TRUE'
+class(x)
+class(y)
+
+a = 5+9i;
+class(a)
+
+a = 'Data Science'
+class(a)
+
+
+airquality<-datasets::airquality
+#top 10 adn last 10 rows
+head(airquality,10)
+tail(airquality,10)
+
+#columns
+airquality[,c(1,2)]
+df<-airquality[,-6]
+df1<-airquality[-153,]
+
+summary(airquality$Ozone)
+airquality$Ozone
+summary(airquality)
+
+
+plot(airquality$Wind)
+plot(airquality$Temp,airquality$Wind,type='p')
+plot(airquality)
+#points & lines
+plot(airquality$Wind,type='l')
+plot(airquality$Ozone, xlab='Ozone Concentration',
+     ylab='No of Instances', main='Ozone levels in NY city',
+     col='blue')
+#Horizontal barplot
+barplot(airquality$Ozone, main='Ozone concentration', 
+        ylab='ozone levels', col='blue', horiz=F, axes = T)
+hist(airquality$Solar.R)
+hist(airquality$Solar.R, main = ' Solar Radiation volumes in air',
+     xlab = 'Solar Radiation', col= 'blue')
+#Boxplot
+boxplot(airquality$Wind, main = 'Boxplot')
+boxplot.stats(airquality$Wind)$out
+#Multiple Boxplot
+boxplot(airquality[,1:4], main = 'Multiple')
+#margin of the grid(mar), 
+#no of rows and columns(mfrow), 
+#whether a border is to be included(bty) 0 - yes, n- no 
+#and position of the 
+#labels(las: 1 for horizontal, las: 0 for vertical)
+#bty - box around the plot
+par(mfrow=c(3,3), mar=c(2,5,2,1), las=0, bty='o')
+plot(airquality$Ozone)
+plot(airquality$Ozone, airquality$Wind)
+plot(airquality$Ozone, type= "l")
+plot(airquality$Ozone, type= "l")
+plot(airquality$Ozone, type= "l")
+barplot(airquality$Ozone, main = 'Ozone Concenteration in air',
+        xlab = 'ozone levels', col='green',horiz = TRUE)
+hist(airquality$Solar.R)
+boxplot(airquality$Solar.R)
+boxplot(airquality[,0:4], main='Multiple Box plots')
+
