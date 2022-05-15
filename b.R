6272c54b01f620bff1f12adbf273f2a7ec897566
manasik29 <97607775+manasik29@users.noreply.github.com>
2022-05-15 13:07:26 +0530

    Add files via upload

---
+++ b.R
@@ -0,0 +1,43 @@
+x=2
+2+2 # defining a value
+x <- 2+2 #storing a value
+x
+a <- 'a'
+a
+a=1
+a1=2
+1a = 3
+a=1L
+class(a)
+a=134.526
+class(a)
+a1.2L
+a <- 1.2L
+s <- 1:10
+s <- seq(1,10)
+s
+s <- seq(1,1000,50)
+s
+1+2i
+x <- 1+2i
+Re(x)
+Im(x)
+class(x)
+a <- 'a'
+class(a)
+x <- LETTERS[1:6]
+x
+x <- letters[1:6]
+x
+LETTERS
+letters
+a <- TRUE
+a
+b <- FALSE
+b
+class(a)
+class(b)
+as.interger(TRUE)
+as.interger(TRUE)
+x <- as.interger(TRUE)
+as.integer()
